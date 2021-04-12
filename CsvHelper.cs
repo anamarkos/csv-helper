74df5c0a978e0ea196a4f2cf24577c61f170efdf
Anastasios Markos <anastasios.markos@iteam.gr>
2021-04-12 14:45:07 +0300

    Add project files.

---
+++ CsvHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CsvHelper
+{
+    public static class CsvHelper
+    {
+        public static void FindLongestCsvColumnData(string file)
+        {
+            var inputLines = File.ReadAllLines(file);
+            Dictionary<int, int> dictIndexLenght = new Dictionary<int, int>();
+            List<string> columnHeaders = inputLines.FirstOrDefault().Split(',').ToList();
+            int maxColumn = 0;
+            int maxRow = 0;
+            string maxValue = "";
+            int maxLength = 0;
+            int lineCounter = 1;// Skips headers
+
+            foreach (var line in inputLines)
+            {
+                lineCounter++;
+                List<string> columList = line.Split(',').ToList();
+
+                for (int i = 0; i < columList.Count; i++)
+                {
+                    int tempVal = 0;
+                    if (dictIndexLenght.TryGetValue(i, out tempVal))
+                    {
+                        if (tempVal < columList[i].Length)
+                        {
+                            dictIndexLenght[i] = columList[i].Length;
+
+                            if (maxLength < columList[i].Length)
+                            {
+                                maxLength = columList[i].Length;
+                                maxColumn = i;
+                                maxRow = lineCounter;
+                                maxValue = columList[i];
+                            }
+                        }
+                    }
+                    else
+                        dictIndexLenght[i] = columList[i].Length;
+                }
+            }
+
+            for (int i = 0; i < dictIndexLenght.Count; i++)
+            {
+                Console.WriteLine("Column {0} : {1}", i, dictIndexLenght[i]);
+            }
+
+            Console.WriteLine($"MaxValue: {maxValue}, MaxRow:{maxRow}, MaxColumnName:{columnHeaders[maxColumn]}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CsvHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
